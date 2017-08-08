6bebefd53df2fbfb87947e79fecaf8c435bf92f8
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-08-08 15:17:30 +0200

    seventh commit

--- public/chat.js
+++ public/chat.js
@@ -13,13 +13,15 @@
 
 
 	function randomize() {
-		let colors = []
+		let colors = ['#f2afaf', '#d7a874', '#7ac0d7','#12d37d','#12bed3', '#c4d312', '#ff76df','#0affd6', '#7cff3e', '#fad60d'];
+		let randomy = Math.floor(Math.random(colors.length) * colors.length)
+		return colors[randomy];
 	}
 
 	//Callback
 	function cb(data) {
 		dataLength.push(data);
-		document.querySelector('#app-inner').innerHTML += `<p><strong>${data.user}: </strong><span>${data.message}</span></p>`;
+		document.querySelector('#app-inner').innerHTML += `<p><strong style=color:${randomize()}>${data.user}: </strong><span style=color:${randomize()}>${data.message}</span></p>`;
 		let para = document.querySelectorAll('#app-inner p');
 		document.title = `Document has (${dataLength.length})`;
 		app.scrollTop = (para[para.length - 1].offsetTop + para[para.length - 1].offsetHeight) + 60;
